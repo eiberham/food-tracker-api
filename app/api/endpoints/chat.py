f1bafe07d1f712b9e2dfe9e8899874c4af8bbe20
eiberham <cedenoabraham@gmail.com>
2025-12-09 00:01:23 -0300

    streaming chat

--- app/api/endpoints/chat.py
+++ app/api/endpoints/chat.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, status, Depends
+from fastapi.responses import StreamingResponse
 from app.api.dependencies.auth import verify_token
 from app.schemas.chat_msg import ChatMsg
 from app.services.chat_service import ChatService
@@ -8,4 +9,5 @@ router = APIRouter()
 
 @router.post("/", status_code=status.HTTP_200_OK)
 async def chat(request: ChatMsg, user=Depends(verify_token), db=Depends(get_db)):
-    return ChatService.chat(db, user.id, request.message)
+    stream = ChatService.chat(db, user.id, request.message)
+    return StreamingResponse(stream, media_type="text/event-stream")
--- app/services/chat_service.py
+++ app/services/chat_service.py
@@ -1,3 +1,13 @@
+"""
+In order to test the streaming endpoint, use this curl command:
+curl -N -X POST 'http://localhost:8000/chat/' \
+  -d '{"message": "What is the goal of a low histamine diet ?"}' \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer JWT_TOKEN' \
+  -H 'Accept: text/event-stream'
+"""
+
+
 from app.llm.agent import create
 from sqlalchemy.orm import Session
 
@@ -8,13 +18,25 @@ class ChatService:
         try:
             
             agent = create(db, user_id)
+
+            def stream():
+                for chunk in agent.stream(
+                    {"messages": [{"role": "user", "content": message}]}, 
+                    config={"configurable": {"thread_id": f"user_{user_id}"}},
+                    stream_mode="updates"
+                ):
+                    yield f"data: {chunk}\n\n"
             
+            """ 
+            Without streaming:
             response = agent.invoke(
                 { "messages": [{"role": "user", "content": message}]},
                 {"configurable": {"thread_id": "1"}}
-            )
-
+            ) 
             return response
+            """
+
+            return stream()
         except Exception as e:
             print(f"Error in ChatService: {e}")
             raise e
